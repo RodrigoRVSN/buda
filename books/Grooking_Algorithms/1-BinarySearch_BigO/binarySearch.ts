76ff274d77774ab139d1c67c611d00e65b78db45
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-07-28 22:49:11 -0300

    feat: add big o notation in binary search

--- books/Grooking_Algorithms/1-BinarySearch_BigO/binarySearch.ts
+++ books/Grooking_Algorithms/1-BinarySearch_BigO/binarySearch.ts
@@ -4,15 +4,15 @@ export const binarySearch = (numbers: number[], number: number) => {
   let middle = numbers.length / 2
 
   while (left <= right) {
-    middle = Math.floor((left + right) / 2)
+    middle = Math.floor((left + right) / 2) // O (log(n))
     const middleNumber = numbers[middle]
 
-    if (middleNumber === number) return true
+    if (middleNumber === number) return true // O (1)
 
-    if (number > middleNumber) {
+    if (number > middleNumber) { // O (1)
       left = middle + 1
     }
-    if (number < middleNumber) {
+    if (number < middleNumber) { // O (1)
       right = middle - 1
     }
   }
