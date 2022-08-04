2db879a6906c4c575eb924579bf64d7616cd2fce
rodrigorvsn <rodrigovictor81@gmail.com>
2022-08-03 23:11:51 -0300

    feat: soli(d) principle description + example

---
+++ SOLID/dependency_inversion_principle/example-good.js
@@ -0,0 +1,40 @@
+class InventoryTracker {
+  constructor(items, requester) {
+    this.items = items;
+    this.requester = requester;
+  }
+
+  requestItems() {
+    this.items.forEach(item => {
+      this.requester.requestItem(item);
+    });
+  }
+}
+
+class InventoryRequesterV1 {
+  constructor() {
+    this.REQ_METHODS = ["HTTP"];
+  }
+
+  requestItem(item) {
+    // ...
+  }
+}
+
+class InventoryRequesterV2 {
+  constructor() {
+    this.REQ_METHODS = ["WS"];
+  }
+
+  requestItem(item) {
+    // ...
+  }
+}
+
+// Constructing our dependencies externally and injecting them, we can easily
+// substitute our request module for another
+const inventoryTracker = new InventoryTracker(
+  ["apples", "bananas"],
+  new InventoryRequesterV2()
+);
+inventoryTracker.requestItems();
