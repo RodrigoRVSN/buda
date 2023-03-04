a249dcdefff86f82361779578769d944256042fc
rodrigorvsn <rodrigovictor81@gmail.com>
2023-03-03 23:53:54 -0300

    fix(cp): correct ChocolateFeast exercise resolution

--- exercises/hackerrank/easy/ChocolateFeast.ts
+++ exercises/hackerrank/easy/ChocolateFeast.ts
@@ -1,10 +1,10 @@
 function chocolateFeast(budget: number, price: number, freeTime: number): number {
   let boughtChocolates = 0
 
-  for (let i = budget; i >= 0; i -= price) {
+  for (let i = budget - price; i >= 0; i -= price) {
     boughtChocolates++
 
-    if (boughtChocolates % freeTime === 0) {
+    if (boughtChocolates > 1 && boughtChocolates % freeTime === 0) {
       boughtChocolates++
     }
   }
