ea4b6d7fe1b2fde82783bdd8f051a9d15f43d558
rodrigorvsn <rodrigovictor81@gmail.com>
2025-05-25 09:56:03 -0300

    feat(go): go-with-tests, Integers

---
+++ docs/exercises/go-with-tests/go.mod
@@ -0,0 +1,5 @@
+module github.com/rodrigorvsn/go-with-tests
+
+go 1.24.1
+
+toolchain go1.24.3
---
+++ docs/exercises/go-with-tests/integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Add takes two integers and return their sum
+func Add(x, y int) int {
+	return x + y
+}
