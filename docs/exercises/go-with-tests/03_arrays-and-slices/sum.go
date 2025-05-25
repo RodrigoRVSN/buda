eb8ab765eb71454fa651bd6dd0bbc512362af8ce
rodrigorvsn <rodrigovictor81@gmail.com>
2025-05-25 10:43:34 -0300

    refactor(go): go-with-tests, arrays and slices

--- docs/exercises/go-with-tests/03_arrays-and-slices/sum.go
+++ docs/exercises/go-with-tests/03_arrays-and-slices/sum.go
@@ -9,10 +9,9 @@ func Sum(values []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	numberOfSums := len(numbersToSum)
-	sums := make([]int, numberOfSums)
-	for i, numbers := range numbersToSum {
-		sums[i] += Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
 }
