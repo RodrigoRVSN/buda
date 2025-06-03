34982773a9a9b79ad0d9d5708534066ef968d319
rodrigorvsn <rodrigovictor81@gmail.com>
2025-06-03 18:16:52 -0300

    feat(go): go-with-tests, dependency injection

---
+++ docs/exercises/go-with-tests/07_dependency_injection/greet.go
@@ -0,0 +1,10 @@
+package greet
+
+import (
+	"fmt"
+	"io"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
---
+++ docs/exercises/go-with-tests/07_dependency_injection/greet_test.go
@@ -0,0 +1,16 @@
+package greet
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+	got := buffer.String()
+	want := "Hello, Chris"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
