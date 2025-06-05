8f9316c06e8c15ddb603f58aec48375488ef32d4
rodrigorvsn <rodrigovictor81@gmail.com>
2025-06-05 18:37:06 -0300

    feat(go): go-with-tests, mocking

---
+++ docs/exercises/go-with-tests/08_mocking/countdown.go
@@ -0,0 +1,28 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, "Go!")
+}
