f8ba0f8a782e73effe9fefd8910c31889bdce4cf
rodrigorvsn <rodrigovictor81@gmail.com>
2025-06-07 07:52:59 -0300

    feat(go): go-with-tests, select

---
+++ docs/exercises/go-with-tests/09_select/racer_test.go
@@ -0,0 +1,43 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speed of the server", func(t *testing.T) {
+		slowServer := makeDelayedServer(10 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't response within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(30 * time.Millisecond)
+
+		_, err := ConfigurableRacer(serverA.URL, serverA.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
