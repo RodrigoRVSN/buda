0de54c514c0432425e307ded45b1db2f1d9bfbc2
rodrigorvsn <rodrigovictor81@gmail.com>
2025-06-22 09:02:59 -0300

    feat(go): go-with-tests, property based

---
+++ docs/exercises/go-with-tests/13_property_based/convert_to_roman.go
@@ -0,0 +1,46 @@
+package convert_to_roman
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic uint16) string {
+	var result strings.Builder // minimizes memory copying
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+	return result.String()
+}
+
+func ConvertToArabic(roman string) uint16 {
+	var arabic uint16 = 0
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+	return arabic
+}
